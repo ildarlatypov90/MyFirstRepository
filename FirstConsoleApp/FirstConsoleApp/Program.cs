751affd9c15637ac9ab7a3bb0c0697c27971e2f0
ildarlatypov90 <ilatypov@kdl.ru>
2025-09-15 17:46:56 +0300

    created FirstConsoleApp project

---
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string profession;
+
+            Console.WriteLine("What's your name?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Your occupation?");
+            profession = Console.ReadLine();
+
+            Greet(name, profession);
+        }
+
+        static void Greet(string name, string profession)
+        {
+            Console.WriteLine("Hello, {0}, {1}.", name, profession);
+        }
+    }
+}
