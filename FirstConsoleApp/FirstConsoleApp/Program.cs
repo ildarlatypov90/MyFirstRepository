488491442f0d696355d8801cb33c81ffdd395012
ildarlatypov90 <ilatypov@kdl.ru>
2026-03-04 16:50:54 +0300

    Базовые классы

--- FirstConsoleApp/FirstConsoleApp/Program.cs
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -4,124 +4,45 @@
     {
         static void Main(string[] args)
         {
-            (string name, string family, int age, bool isHavePet,
-                 int petCount, string[] petsNames, string[] colors) Anketa;
 
-            Anketa = Program.GetAnketa();
 
-            Program.ShowAnketa(Anketa);
-
-            Console.ReadKey();
+        }
+        abstract class Delivery
+        {
+            public string Address;
         }
 
-        static (string name, string family, int age, bool isHavePet,
-                 int petCount, string[] petsNames, string[] colors) GetAnketa()
+        class HomeDelivery : Delivery
         {
-            string name     = "";
-            string family   = "";
-            int age         = 0;
-            bool isHavePet  = false;
-            int petCount    = 0;
-            string[] petsNames  = new string[0];
-            string[] colors     = new string[0];
-
-            Console.WriteLine("Введите ваше имя:");
-            name = Console.ReadLine();
-
-            Console.WriteLine("Введите вашу фамилию:");
-            family = Console.ReadLine();
-
-            string ageR;
-            do
-            {
-                Console.WriteLine("Введите ваш возраст цифрами:");
-                ageR = Console.ReadLine();
-            } while (!Program.CheckInt(ageR, out age));
-
-            string answer = "";
-            do
-            {
-
-                Console.WriteLine("Есть ли у вас питомец/питомцы (да/нет):");
-                answer = Console.ReadLine().ToLower();
-                if (answer == "да")
-                    isHavePet = true;
-            } while (answer!="да" && answer != "нет");
-
-            if (isHavePet)
-            {
-                string petCountR;
-                do
-                {
-                    Console.WriteLine("Сколько у вас питомцев цифрами:");
-                    petCountR = Console.ReadLine();
-                } while (!Program.CheckInt(petCountR, out petCount));
-
-                petsNames = new string[petCount];
-                Console.WriteLine("Введите имена питомцев.");
-                for (int i = 0; i < petCount; i++)
-                {
-                    Console.WriteLine("Имя питомца {0}", i + 1);
-                    petsNames[i] = Console.ReadLine();
-                }
-            }
-
-            int colorsCount;
-            string colorsCountR;
-            do
-            {
-                Console.WriteLine("Введите количество любимых цветов цифрами:");
-                colorsCountR = Console.ReadLine();
-            } while (!Program.CheckInt(colorsCountR, out colorsCount));
-
-            colors = Program.GetColors(colorsCount);
-
-            return (name, family, age, isHavePet,
-                petCount, petsNames, colors);
+            /* ... */
         }
 
-        static string[] GetColors(int colorsCount)
+        class PickPointDelivery : Delivery
         {
-            string[] colors = new string[colorsCount];
-
-            for (int i=0; i < colorsCount; i++)
-            {
-                Console.WriteLine("Введите любимый {0} цвет", i + 1);
-                colors[i] = Console.ReadLine();
-            }
-
-            return colors;
+            /* ... */
         }
 
-        static bool CheckInt(string inputParam, out int corrNumber)
+        class ShopDelivery : Delivery
         {
-            int intNum;
-            
-            if (int.TryParse(inputParam, out intNum))
-            {
-                if (intNum > 0)
-                {
-                    corrNumber = intNum;
-                    return true;
-                }
-            }
-
-            corrNumber = 0;
-            return false;
+            /* ... */
         }
 
-        static void ShowAnketa((string name, string family, int age, bool isHavePet,
-                 int petCount, string[] petsNames, string[] colors) Anketa)
+        class Product
+        { }
+
+        class Order<TDelivery, TStruct> where TDelivery : Delivery
         {
-            Console.WriteLine("Вас зовут {0} {1}.", Anketa.family, Anketa.name);
-            Console.WriteLine("Ваш возраст {0}.", Anketa.age);
-            if (Anketa.petsNames.Length>0)
+            public TDelivery Delivery;
+
+            public int Number;
+
+            public string Description;
+
+            private Product[] products;
+
+            public void DisplayAddress()
             {
-                Console.WriteLine("У вас есть питомцы: {0}", string.Join(", ", Anketa.petsNames));
-            }
-            if (Anketa.colors.Length>0)
-            {
-                Console.WriteLine("Ваши любимые цвета: {0}", string.Join(", ", Anketa.colors));
+                Console.WriteLine(Delivery.Address);
             }
         }
     }
