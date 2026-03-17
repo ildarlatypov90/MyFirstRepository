b37753c55513a7130012df8ffc84b62cad7e066c
ildarlatypov90 <ilatypov@kdl.ru>
2026-03-17 12:38:23 +0300

    Наверное выполнен Продвинутый уровень

--- FirstConsoleApp/FirstConsoleApp/Program.cs
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -5,77 +5,110 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
+            Book b = new Book("1001 способ написать говнокод");
+            Console.WriteLine(b.type, b.description);
 
-
+            Console.ReadKey();
         }
         abstract class Delivery
         {
-            public string Address { get; set; }
+            public string address { get; set; }
         }
 
         class HomeDelivery : Delivery
         {
+            protected DateTime date; // время доставки
+            protected string address { get; set; }
             /* ... */
+            public HomeDelivery(string iHomeAddress, DateTime iDate)
+            {
+                address = iHomeAddress;
+                date = iDate;
+            }
         }
 
         class PickPointDelivery : Delivery
         {
+            public string[] addresses = {
+                "ул. Валидольная 4",
+                "ул. Карвалольная 5",
+                "ул. Ленина 1" };
             
-            /* ... */
+            protected string address {
+                get
+                {
+                    // вернуть ближайший к пользователю, для теста верну просто 0.
+                    return addresses[0];
+                }
+            }
         }
 
         class ShopDelivery : Delivery
         {
-            /* ... */
+            public static TimeOnly startDate = new TimeOnly(9,0);
+            public static TimeOnly endDate = new TimeOnly(22,0);
+
+            static public void ShowWorkHours()
+            {
+                Console.Write("Время работы {0} - {1}", ShopDelivery.startDate, ShopDelivery.endDate);
+            }
         }
 
         abstract class Product
         {
-            private string description { get; }
+            public string type { get; }
+            public string description { get; set; }
         }
 
         class Book : Product
         {
-            private string description = "Книга";
-            // только для чтения
-            public string GetDescription()
+            public static string iType = "Книга";
+            public string type
             {
-                return description;
+                get
+                {
+                    return Book.iType;
+                }
+            }
+
+            public Book(string iDescription)
+            {
+                description = iDescription;
             }
         }
 
         class Order<TDelivery, TStruct> where TDelivery : Delivery
         {
-            public TDelivery Delivery;
+            public TDelivery delivery;
 
             public int Number;
 
             public string Description;
 
-            private List<Product> products;
+            protected List<Product> products;
 
             public Order()
             {
-                this.Delivery = null;
+                this.delivery = null;
                 this.products = new List<Product>();
             }
 
             public Order(TDelivery inputDelivery)
             {
-                this.Delivery = inputDelivery;
+                this.delivery = inputDelivery;
                 this.products = new List<Product>();
             }
 
             public Order(TDelivery inputDelivery, Product inputProduct)
             {
-                this.Delivery = inputDelivery;
+                this.delivery = inputDelivery;
                 this.products = new List<Product>();
                 this.products.Append(inputProduct);
             }
 
             public void DisplayAddress()
             {
-                Console.WriteLine(Delivery.Address);
+                Console.WriteLine(delivery.address);
             }
             
             public void AddProduct(Product product)
