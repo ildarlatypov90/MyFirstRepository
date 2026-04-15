95599d750d0176bd1dd4036fed64e93b2c0ce4b6
ildarlatypov90 <ilatypov@kdl.ru>
2026-04-15 16:01:06 +0300

    delegate example

--- FirstConsoleApp/FirstConsoleApp/Program.cs
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace FirstConsoleApp
 {
-    internal class Program
+    public class Program
     {
+        public delegate int SumDelegate(int a, int b, int c);
+
         static void Main(string[] args)
         {
+            SumDelegate sumDelegate = Sum;
+            sumDelegate.Invoke(1, 10, 50);
+            Console.ReadKey();
+        }
 
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
         }
     }
 }
