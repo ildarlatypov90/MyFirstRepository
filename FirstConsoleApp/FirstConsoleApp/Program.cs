c6b7277e93f069b8480aad9cf3148e3eaef4ea5f
ildarlatypov90 <ilatypov@kdl.ru>
2026-04-15 15:50:54 +0300

    done

--- FirstConsoleApp/FirstConsoleApp/Program.cs
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
+            try
+            {
+                throw new ArgumentOutOfRangeException("Message for ArgumentOutOfRangeException");
+                // throw new RankException("Message for RankException");
+            }
+            catch (Exception ex) when (ex is ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Is ArgumentOutOfRangeException exception");
+            }
+            catch (Exception ex) when (ex is RankException)
+            {
+                Console.WriteLine("Is RankException exception");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Is {0} exception", ex.GetType());
+            }
 
-        }
+            Console.ReadKey();
+            }
+
     }
 }
